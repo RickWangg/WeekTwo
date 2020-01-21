f413073253baac116c2dd4d2fe4ac33058644024
Yuk Chuen Rick Wang <rickwangg@gmail.com>
2020-01-21 01:07:40 -0500

    Initial Commit

---
+++ Two-Button Challenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Two-Button Challenge
+//
+//  Created by Yuk Chuen Rick Wang on 1/21/20.
+//  Copyright © 2020 Yuk Chuen Rick Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
